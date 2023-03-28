1016d0a4980dc1fa8ff7ebc86a02abc8b377f6f4
Fabio Diogo <fabindiogo@gmail.com>
2023-03-27 21:11:22 -0300

    imagens no header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,6 +7,9 @@ const Header = () => {
     <header className={styles.header}>
       <div className={styles.div1}>
         <h1>SHOPPING</h1>
+        <img src="facebook-24.png" alt="" />
+        <img src="instagram-24.png" alt="" />
+        <img src="whatsapp-30.png" alt="" />
         <div className={styles.searchBar}>
           <img src="search-24.png" alt="" />
           <input type="text" placeholder="Pesquisar" />
