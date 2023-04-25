eb48b404efb9f736e5735062f87a0b2438fb5110
Fabio Diogo <fabindiogo@gmail.com>
2023-04-24 22:38:31 -0300

    estilização da pagina de poltronas

--- src/routes/Poltronas.tsx
+++ src/routes/Poltronas.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 
-import PoltronaOption from "../components/PoltronaOption"
 import styles from "../Poltronas.module.css"
 import Header2 from '../components/Header2';
 export function Poltronas(){
   return(
-    <div className={styles.sala}>
+    <div className={styles.page}>
       <Header2 />
+      <body className={styles.body}>
+        <div className={styles.container}>
+          <div className={styles.div1}>
+            <h1>Escola xx poltrona(s)</h1>
+            <p>Selecione seu(s) lugar(es) clicando na poltrona de acordo a legenda.</p>
+            <p>Para desmarcar seu(s) lugar(es) clique novamente sobre a poltrona(s) reservada(s).</p>
+          </div>
+          <div className={styles.div2}>
+          
+          </div>
+        </div>        
+      </body>
     </div>
   )
 }
